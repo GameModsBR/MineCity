7456bf5e6f0f64d0034abdc1b1de7df741de82ce
joserobjr <jose.rob.jr@gmail.com>
2016-08-13 10:12:24 -0300

    Implements commands to check details about a permission and to toggle the modify flag

--- Core/src/main/java/br/com/gamemods/minecity/commands/PermissionCommands.java
+++ Core/src/main/java/br/com/gamemods/minecity/commands/PermissionCommands.java
@@ -31,6 +31,89 @@ public class PermissionCommands
         this.mineCity = mineCity;
     }
 
+    private Message list(ExceptFlagHolder holder, PermissionFlag flag, String prefix, String name)
+    {
+        Optional<Message> def = holder.can(flag);
+        Map<Identity<?>, Message> exceptions = holder.getDirectPermissions(flag);
+        String flagKey = flag.name().toLowerCase().replace('_', '-');
+        if(exceptions.isEmpty() || exceptions.values().stream().noneMatch(
+                m->
+                   m == null && def.isPresent()
+                || m != null && !def.isPresent()
+                || m != null && def.isPresent() && !m.equals(def.get())
+        ))
+        {
+            if(def.isPresent())
+                return new Message(
+                        prefix+".nobody."+ flagKey +".no-exception",
+                        "<msg><red><b>Nobody</b></red> can do that on ${name}, that will be shown when that happens:<br/><red>${msg}</red></msg>",
+                        new Object[][]{
+                                {"name", name},
+                                {"msg", def.get()}
+                        }
+                );
+            else
+                return new Message(
+                        prefix+".everybody."+ flagKey +".no-exception",
+                        "<msg><green><b>Everybody</b></green> can do that on ${name}</msg>",
+                        new Object[]{"name", name}
+                );
+        }
+
+        Message except = Message.list(
+                exceptions.entrySet().stream().map(e->
+                        new Message(
+                                prefix+".exceptions."+e.getKey().getType().name().toLowerCase().replace('_','-') +
+                                        (e.getValue() == null?".allow" :".deny")
+                                ,
+                                e.getValue() == null?
+                                        "<msg><green><b>${name}</b> - Is allowed</green></msg>" :
+                                        "<msg><red><b>${name}</b> - Is denied with: ${msg}</red></msg>",
+                                new Object[][]{
+                                        {"msg", e.getValue()},
+                                        {"name",
+                                                e.getKey().getType() != Identity.Type.GROUP?
+                                                        e.getKey().getName() :
+                                                        new Message(
+                                                                prefix+".exceptions.group.name",
+                                                                "${group} from ${city}",
+                                                                new Object[][]{
+                                                                        {"group", e.getKey().getName()},
+                                                                        {"city", ((GroupID)e.getKey()).home}
+                                                                }
+                                                        )
+                                        }}
+                        )).toArray(Message[]::new)
+                ,
+                new Message("", "\n"));
+
+        if(def.isPresent())
+            return new Message(
+                    prefix+".nobody."+flagKey+".except",
+                    "<msg><red><b>Nobody</b></red> <yellow>(${count} exceptions)</yellow> can do that on ${name}, that will be shown when that happens:" +
+                            "<br/><red>${msg}</red><br/><br/>" +
+                            "<yellow>Exceptions:</yellow><br/>${exceptions}</msg>",
+                    new Object[][]{
+                            {"name", name},
+                            {"msg", def.get()},
+                            {"exceptions", except},
+                            {"count", exceptions.size()}
+                    }
+            );
+        else
+            return new Message(
+                    prefix+".everybody."+flagKey+".except",
+                    "<msg><green><b>Everybody</b></green> <yellow>(${count} exceptions)</yellow> can do that on ${name}"+
+                            "<br/><br/>" +
+                            "<yellow>Exceptions:</yellow><br/>${exceptions}</msg>",
+                    new Object[][]{
+                            {"name", name},
+                            {"exceptions", except},
+                            {"count", exceptions.size()}
+                    }
+            );
+    }
+
     private Message[] list(ExceptFlagHolder holder, Set<PermissionFlag> filter, String prefix, String name)
     {
         Message[] lines = new Message[2 + filter.size()];
@@ -116,6 +199,70 @@ public class PermissionCommands
         );
 
         return lines;
+    }
+
+    public CommandResult<?> list(CommandEvent cmd, PermissionFlag flag) throws DataSourceException
+    {
+        City city;
+        if(cmd.args.isEmpty())
+        {
+            city = cmd.getChunk().getCity().orElse(null);
+            if(city == null)
+                return new CommandResult<>(new Message("cmd.city.perms.not-claimed", "You are not inside a city"));
+        }
+        else
+        {
+            String name = String.join(" ", cmd.args);
+            city = mineCity.dataSource.getCityByName(name).orElse(null);
+            if(city == null)
+                return new CommandResult<>(new Message(
+                        "cmd.city.perms.not-found",
+                        "No city were found with name ${name}",
+                        new Object[]{"name", name}
+                ));
+        }
+
+        return new CommandResult<>(list(city, flag, "cmd.city.perms", city.getName()), true);
+    }
+
+    public CommandResult<?> listPlot(CommandEvent cmd, PermissionFlag flag) throws DataSourceException
+    {
+        Plot plot;
+        if(cmd.args.isEmpty())
+        {
+            if(cmd.position == null)
+                return new CommandResult<>(new Message(
+                        "cmd.plot.perms.type-city",
+                        "Type a city name"
+                ));
+
+            plot = cmd.getChunk().getPlotAt(cmd.position.getBlock()).orElse(null);
+            if(plot == null)
+                return new CommandResult<>(new Message(
+                        "cmd.plot.perms.not-claimed",
+                        "You are not inside a plot"
+                ));
+        }
+        else
+        {
+            plot = cmd.getChunk().getCity().flatMap(c-> c.getPlot(String.join(" ", cmd.args))).orElse(null);
+            if(plot == null && cmd.args.size() > 1)
+            {
+                plot = mineCity.dataSource.getCityByName(cmd.args.get(0))
+                        .flatMap(c-> c.getPlot(String.join(" ", cmd.args.subList(1, cmd.args.size()))))
+                        .orElse(null)
+                ;
+            }
+
+            if(plot == null)
+                return new CommandResult<>(new Message(
+                        "cmd.plot.perms.not-found",
+                        "No plot was found with ${search}",
+                        new Object[]{"search", String.join(" ", cmd.args)}
+                ));
+        }
+
+        return new CommandResult<>(list(plot, flag, "cmd.plot.perms", plot.getName()), true);
     }
 
     @Slow
@@ -657,6 +804,139 @@ public class PermissionCommands
 
     @Slow
     @Async
+    @Command(value = "city.perms.enter", args = @Arg(name = "city", sticky = true, optional = true, type = Arg.Type.CITY))
+    public CommandResult<?> listEnter(CommandEvent cmd) throws DataSourceException
+    {
+        return list(cmd, PermissionFlag.ENTER);
+    }
+
+    @Slow
+    @Async
+    @Command(value = "city.perms.click", args = @Arg(name = "city", sticky = true, optional = true, type = Arg.Type.CITY))
+    public CommandResult<?> listClick(CommandEvent cmd) throws DataSourceException
+    {
+        return list(cmd, PermissionFlag.CLICK);
+    }
+
+    @Slow
+    @Async
+    @Command(value = "city.perms.pickup", args = @Arg(name = "city", sticky = true, optional = true, type = Arg.Type.CITY))
+    public CommandResult<?> listPickup(CommandEvent cmd) throws DataSourceException
+    {
+        return list(cmd, PermissionFlag.PICKUP);
+    }
+
+    @Slow
+    @Async
+    @Command(value = "city.perms.open", args = @Arg(name = "city", sticky = true, optional = true, type = Arg.Type.CITY))
+    public CommandResult<?> listOpen(CommandEvent cmd) throws DataSourceException
+    {
+        return list(cmd, PermissionFlag.OPEN);
+    }
+
+    @Slow
+    @Async
+    @Command(value = "city.perms.pvp", args = @Arg(name = "city", sticky = true, optional = true, type = Arg.Type.CITY))
+    public CommandResult<?> listPVP(CommandEvent cmd) throws DataSourceException
+    {
+        return list(cmd, PermissionFlag.PVP);
+    }
+
+    @Slow
+    @Async
+    @Command(value = "city.perms.pvc", args = @Arg(name = "city", sticky = true, optional = true, type = Arg.Type.CITY))
+    public CommandResult<?> listPVC(CommandEvent cmd) throws DataSourceException
+    {
+        return list(cmd, PermissionFlag.PVC);
+    }
+
+    @Slow
+    @Async
+    @Command(value = "city.perms.modify", args = @Arg(name = "city", sticky = true, optional = true, type = Arg.Type.CITY))
+    public CommandResult<?> listModify(CommandEvent cmd) throws DataSourceException
+    {
+        return list(cmd, PermissionFlag.MODIFY);
+    }
+
+    @Slow
+    @Async
+    @Command(value = "plot.perms.enter", args = {
+            @Arg(name = "city-or-plot", optional = true, type = Arg.Type.PLOT_OR_CITY),
+            @Arg(name = "plot", sticky = true, optional = true, type = Arg.Type.PLOT)
+    })
+    public CommandResult<?> listPlotEnter(CommandEvent cmd) throws DataSourceException
+    {
+        return listPlot(cmd, PermissionFlag.ENTER);
+    }
+
+    @Slow
+    @Async
+    @Command(value = "plot.perms.click", args = {
+            @Arg(name = "city-or-plot", optional = true, type = Arg.Type.PLOT_OR_CITY),
+            @Arg(name = "plot", sticky = true, optional = true, type = Arg.Type.PLOT)
+    })
+    public CommandResult<?> listPlotClick(CommandEvent cmd) throws DataSourceException
+    {
+        return listPlot(cmd, PermissionFlag.CLICK);
+    }
+
+    @Slow
+    @Async
+    @Command(value = "plot.perms.pickup", args = {
+            @Arg(name = "city-or-plot", optional = true, type = Arg.Type.PLOT_OR_CITY),
+            @Arg(name = "plot", sticky = true, optional = true, type = Arg.Type.PLOT)
+    })
+    public CommandResult<?> listPlotPickup(CommandEvent cmd) throws DataSourceException
+    {
+        return listPlot(cmd, PermissionFlag.PICKUP);
+    }
+
+    @Slow
+    @Async
+    @Command(value = "plot.perms.open", args = {
+            @Arg(name = "city-or-plot", optional = true, type = Arg.Type.PLOT_OR_CITY),
+            @Arg(name = "plot", sticky = true, optional = true, type = Arg.Type.PLOT)
+    })
+    public CommandResult<?> listPlotOpen(CommandEvent cmd) throws DataSourceException
+    {
+        return listPlot(cmd, PermissionFlag.OPEN);
+    }
+
+    @Slow
+    @Async
+    @Command(value = "plot.perms.pvp", args = {
+            @Arg(name = "city-or-plot", optional = true, type = Arg.Type.PLOT_OR_CITY),
+            @Arg(name = "plot", sticky = true, optional = true, type = Arg.Type.PLOT)
+    })
+    public CommandResult<?> listPlotPVP(CommandEvent cmd) throws DataSourceException
+    {
+        return listPlot(cmd, PermissionFlag.PVP);
+    }
+
+    @Slow
+    @Async
+    @Command(value = "plot.perms.pvc", args = {
+            @Arg(name = "city-or-plot", optional = true, type = Arg.Type.PLOT_OR_CITY),
+            @Arg(name = "plot", sticky = true, optional = true, type = Arg.Type.PLOT)
+    })
+    public CommandResult<?> listPlotPVC(CommandEvent cmd) throws DataSourceException
+    {
+        return listPlot(cmd, PermissionFlag.PVC);
+    }
+
+    @Slow
+    @Async
+    @Command(value = "plot.perms.modify", args = {
+            @Arg(name = "city-or-plot", optional = true, type = Arg.Type.PLOT_OR_CITY),
+            @Arg(name = "plot", sticky = true, optional = true, type = Arg.Type.PLOT)
+    })
+    public CommandResult<?> listPlotModify(CommandEvent cmd) throws DataSourceException
+    {
+        return listPlot(cmd, PermissionFlag.MODIFY);
+    }
+
+    @Slow
+    @Async
     @Command(value = "city.deny.enter", console = false,
             args = {@Arg(name = "player or city", type = Arg.Type.PLAYER_OR_CITY, optional = true),
                     @Arg(name = "group name", type = Arg.Type.GROUP, relative = "player or city", optional = true),
@@ -730,6 +1010,18 @@ public class PermissionCommands
 
     @Slow
     @Async
+    @Command(value = "city.deny.modify", console = false,
+            args = {@Arg(name = "player or city", type = Arg.Type.PLAYER_OR_CITY, optional = true),
+                    @Arg(name = "group name", type = Arg.Type.GROUP, relative = "player or city", optional = true),
+                    @Arg(name = "reason", sticky = true, optional = true)
+            })
+    public CommandResult<?> denyModify(CommandEvent cmd) throws DataSourceException
+    {
+        return deny(cmd, PermissionFlag.MODIFY);
+    }
+
+    @Slow
+    @Async
     @Command(value = "city.allow.enter", console = false,
             args = {@Arg(name = "player or city", type = Arg.Type.PLAYER_OR_CITY, optional = true),
                     @Arg(name = "group name", type = Arg.Type.GROUP, relative = "player or city", optional = true)
@@ -799,6 +1091,17 @@ public class PermissionCommands
 
     @Slow
     @Async
+    @Command(value = "city.allow.modify", console = false,
+            args = {@Arg(name = "player or city", type = Arg.Type.PLAYER_OR_CITY, optional = true),
+                    @Arg(name = "group name", type = Arg.Type.GROUP, relative = "player or city", optional = true)
+            })
+    public CommandResult<?> allowModify(CommandEvent cmd) throws DataSourceException
+    {
+        return allow(cmd, PermissionFlag.MODIFY);
+    }
+
+    @Slow
+    @Async
     @Command(value = "city.allow.all.enter", console = false)
     public CommandResult<?> allowAllEnter(CommandEvent cmd)
     {
@@ -847,6 +1150,14 @@ public class PermissionCommands
 
     @Slow
     @Async
+    @Command(value = "city.allow.all.modify", console = false)
+    public CommandResult<?> allowAllModify(CommandEvent cmd)
+    {
+        return allowAll(cmd, PermissionFlag.MODIFY);
+    }
+
+    @Slow
+    @Async
     @Command(value = "city.deny.all.enter", console = false, args = @Arg(name = "reason", sticky = true, optional = true))
     public CommandResult<?> denyAllEnter(CommandEvent cmd)
     {
@@ -891,6 +1202,14 @@ public class PermissionCommands
     public CommandResult<?> denyAllPVC(CommandEvent cmd)
     {
         return denyAll(cmd, PermissionFlag.PVC);
+    }
+
+    @Slow
+    @Async
+    @Command(value = "city.deny.all.modify", console = false, args = @Arg(name = "reason", sticky = true, optional = true))
+    public CommandResult<?> denyAllModify(CommandEvent cmd)
+    {
+        return denyAll(cmd, PermissionFlag.MODIFY);
     }
 
     @Slow
@@ -968,6 +1287,18 @@ public class PermissionCommands
 
     @Slow
     @Async
+    @Command(value = "plot.deny.modify", console = false,
+            args = {@Arg(name = "player or city", type = Arg.Type.PLAYER_OR_CITY, optional = true),
+                    @Arg(name = "group name", type = Arg.Type.GROUP, relative = "player or city", optional = true),
+                    @Arg(name = "reason", sticky = true, optional = true)
+            })
+    public CommandResult<?> denyModifyPlot(CommandEvent cmd) throws DataSourceException
+    {
+        return denyPlot(cmd, PermissionFlag.MODIFY);
+    }
+
+    @Slow
+    @Async
     @Command(value = "plot.allow.enter", console = false,
             args = {@Arg(name = "player or city", type = Arg.Type.PLAYER_OR_CITY, optional = true),
                     @Arg(name = "group name", type = Arg.Type.GROUP, relative = "player or city", optional = true)
@@ -1037,6 +1368,17 @@ public class PermissionCommands
 
     @Slow
     @Async
+    @Command(value = "plot.allow.modify", console = false,
+            args = {@Arg(name = "player or city", type = Arg.Type.PLAYER_OR_CITY, optional = true),
+                    @Arg(name = "group name", type = Arg.Type.GROUP, relative = "player or city", optional = true)
+            })
+    public CommandResult<?> allowModifyPlot(CommandEvent cmd) throws DataSourceException
+    {
+        return allowPlot(cmd, PermissionFlag.MODIFY);
+    }
+
+    @Slow
+    @Async
     @Command(value = "plot.allow.all.enter", console = false)
     public CommandResult<?> allowAllEnterPlot(CommandEvent cmd)
     {
@@ -1085,6 +1427,14 @@ public class PermissionCommands
 
     @Slow
     @Async
+    @Command(value = "plot.allow.all.modify", console = false)
+    public CommandResult<?> allowAllModifyPlot(CommandEvent cmd)
+    {
+        return allowAllPlot(cmd, PermissionFlag.MODIFY);
+    }
+
+    @Slow
+    @Async
     @Command(value = "plot.deny.all.enter", console = false, args = @Arg(name = "reason", sticky = true, optional = true))
     public CommandResult<?> denyAllEnterPlot(CommandEvent cmd)
     {
@@ -1129,5 +1479,13 @@ public class PermissionCommands
     public CommandResult<?> denyAllPVCPlot(CommandEvent cmd)
     {
         return denyAllPlot(cmd, PermissionFlag.PVC);
+    }
+
+    @Slow
+    @Async
+    @Command(value = "plot.deny.all.modify", console = false, args = @Arg(name = "reason", sticky = true, optional = true))
+    public CommandResult<?> denyAllModifyPlot(CommandEvent cmd)
+    {
+        return denyAllPlot(cmd, PermissionFlag.MODIFY);
     }
 }
