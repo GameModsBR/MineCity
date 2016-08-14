4600dc253401d7a7681bb37906f14d7289a9e6a0
joserobjr <jose.rob.jr@gmail.com>
2016-08-14 18:12:47 -0300

    Bukkit: Implements many protections related to entities

--- Bukkit/src/main/java/br/com/gamemods/minecity/bukkit/VanillaProtections.java
+++ Bukkit/src/main/java/br/com/gamemods/minecity/bukkit/VanillaProtections.java
@@ -1,37 +1,59 @@
 package br.com.gamemods.minecity.bukkit;
 
+import br.com.gamemods.minecity.api.PlayerID;
 import br.com.gamemods.minecity.api.command.Message;
 import br.com.gamemods.minecity.api.permission.FlagHolder;
 import br.com.gamemods.minecity.api.permission.PermissionFlag;
 import br.com.gamemods.minecity.api.world.BlockPos;
+import br.com.gamemods.minecity.api.world.ChunkPos;
 import br.com.gamemods.minecity.bukkit.command.BukkitPlayer;
+import br.com.gamemods.minecity.structure.City;
 import br.com.gamemods.minecity.structure.ClaimedChunk;
+import br.com.gamemods.minecity.structure.Island;
+import br.com.gamemods.minecity.structure.Plot;
+import com.google.common.collect.MapMaker;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.block.Block;
 import org.bukkit.entity.*;
 import org.bukkit.entity.minecart.ExplosiveMinecart;
 import org.bukkit.entity.minecart.PoweredMinecart;
 import org.bukkit.entity.minecart.RideableMinecart;
-import org.bukkit.entity.minecart.SpawnerMinecart;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.ProjectileHitEvent;
+import org.bukkit.event.player.PlayerInteractAtEntityEvent;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.event.vehicle.VehicleDamageEvent;
 import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.potion.PotionEffectType;
+import org.bukkit.projectiles.BlockProjectileSource;
+import org.bukkit.projectiles.ProjectileSource;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Optional;
+import java.util.Set;
 import java.util.function.Supplier;
 
 import static br.com.gamemods.minecity.api.CollectionUtil.optionalStream;
 import static br.com.gamemods.minecity.api.permission.FlagHolder.can;
 import static br.com.gamemods.minecity.api.permission.FlagHolder.wrapDeny;
+import static br.com.gamemods.minecity.api.permission.PermissionFlag.*;
 import static br.com.gamemods.minecity.bukkit.BukkitUtil.optional;
 
 public class VanillaProtections implements Listener
 {
+    private Set<Egg> eggs = Collections.newSetFromMap(new MapMaker().weakKeys().makeMap());
+    private Set<EnderPearl> pearls = Collections.newSetFromMap(new MapMaker().weakKeys().makeMap());
+
     @NotNull
     private final MineCityBukkit plugin;
 
@@ -66,6 +88,242 @@ public class VanillaProtections implements Listener
         return false;
     }
 
+    @Nullable
+    private <E extends Entity> E getNearest(@NotNull Set<E> set, @NotNull Location loc)
+    {
+        double best = Double.MAX_VALUE;
+        E nearest = null;
+        for(E entity : set)
+        {
+            double dist = loc.distanceSquared(entity.getLocation());
+            if(dist < best)
+            {
+                best = dist;
+                nearest = entity;
+            }
+        }
+
+        return nearest;
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
+    public void onCreatureSpawn(CreatureSpawnEvent event)
+    {
+        CreatureSpawnEvent.SpawnReason reason = event.getSpawnReason();
+        switch(reason)
+        {
+            case ENDER_PEARL:
+            case EGG:
+            {
+                Location location = event.getLocation();
+                Set<? extends Projectile> set = reason == CreatureSpawnEvent.SpawnReason.EGG? eggs : pearls;
+                Projectile entity = getNearest(set, location);
+                if(entity == null)
+                {
+                    event.setCancelled(true);
+                    return;
+                }
+
+                ProjectileSource shooter = entity.getShooter();
+                if(shooter instanceof Player)
+                {
+                    if(check(location, (Player) shooter, PermissionFlag.MODIFY))
+                    {
+                        event.setCancelled(true);
+                        return;
+                    }
+                }
+            }
+            break;
+        }
+    }
+
+    @EventHandler(priority = EventPriority.LOW)
+    public void onProjectHit(ProjectileHitEvent event)
+    {
+        Projectile entity = event.getEntity();
+        switch(entity.getType())
+        {
+            case EGG:
+            {
+                Egg egg = (Egg) entity;
+                eggs.add(egg);
+                plugin.plugin.getScheduler().runTaskLater(plugin.plugin, () -> eggs.remove(egg), 5);
+            }
+            break;
+            case ENDER_PEARL:
+            {
+                EnderPearl pearl = (EnderPearl) entity;
+                Location location = pearl.getLocation();
+
+                ProjectileSource shooter = pearl.getShooter();
+                if(shooter instanceof Player)
+                {
+                    if(check(location, (Player) shooter, PermissionFlag.ENTER))
+                    {
+                        pearl.remove();
+                        return;
+                    }
+                }
+
+                pearls.add(pearl);
+                plugin.plugin.getScheduler().runTaskLater(plugin.plugin, () -> pearls.remove(pearl), 5);
+            }
+            break;
+        }
+    }
+
+    @EventHandler(priority = EventPriority.LOW, ignoreCancelled = true)
+    public void onVehicleDamage(VehicleDamageEvent event)
+    {
+        Entity attacker = event.getAttacker();
+        Vehicle vehicle = event.getVehicle();
+
+        if(attacker instanceof Projectile)
+        {
+            ProjectileSource shooter = ((Projectile)attacker).getShooter();
+
+            if(shooter instanceof Entity)
+                attacker = (Entity) shooter;
+            else if(shooter instanceof BlockProjectileSource)
+            {
+                Block block = ((BlockProjectileSource) shooter).getBlock();
+                Location loc = block.getLocation();
+                ChunkPos chunkPos = plugin.chunk(loc);
+                ClaimedChunk chunk = plugin.mineCity.provideChunk(chunkPos);
+                PlayerID owner = chunk.getPlotAt(loc.getBlockX(), loc.getBlockY(), loc.getBlockZ()).map(Plot::owner)
+                        .orElseGet(()-> chunk.getIsland().map(Island::getCity).map(City::getOwner).orElse(null));
+
+                if(owner == null)
+                    return;
+
+                BlockPos blockPos = plugin.blockPos(vehicle.getLocation());
+                FlagHolder holder;
+                if(blockPos.getChunk().equals(chunkPos))
+                    holder = chunk.getFlagHolder(blockPos);
+                else
+                    holder = plugin.mineCity.provideChunk(blockPos.getChunk()).getFlagHolder(blockPos);
+
+                Optional<Message> denial = holder.can(owner, PermissionFlag.MODIFY);
+                if(denial.isPresent())
+                {
+                    event.setCancelled(true);
+                    return;
+                }
+            }
+        }
+
+        if(attacker instanceof Player)
+        {
+            if(check(vehicle.getLocation(), (Player)attacker, PermissionFlag.MODIFY))
+                event.setCancelled(true);
+        }
+    }
+
+    @EventHandler(priority = EventPriority.LOW)
+    public void onEntityDamageByEntity(EntityDamageByEntityEvent event)
+    {
+        Entity attacker = event.getDamager();
+        switch(attacker.getType())
+        {
+            case EGG:
+                Egg egg = (Egg) attacker;
+                eggs.add(egg);
+                plugin.plugin.getScheduler().runTaskLater(plugin.plugin, () -> eggs.remove(egg), 5);
+                break;
+
+            case ENDER_PEARL:
+                EnderPearl pearl = (EnderPearl) pearls;
+                pearls.add(pearl);
+                plugin.plugin.getScheduler().runTaskLater(plugin.plugin, () -> pearls.remove(pearl), 5);
+                break;
+        }
+
+        if(event.isCancelled())
+            return;
+
+        if(attacker instanceof Projectile)
+        {
+            ProjectileSource shooter = ((Projectile)attacker).getShooter();
+
+            if(shooter instanceof Entity)
+                attacker = (Entity) shooter;
+            else if(shooter instanceof BlockProjectileSource)
+            {
+                Block block = ((BlockProjectileSource) shooter).getBlock();
+                Location loc = block.getLocation();
+                ChunkPos chunkPos = plugin.chunk(loc);
+                ClaimedChunk chunk = plugin.mineCity.provideChunk(chunkPos);
+                PlayerID owner = chunk.getPlotAt(loc.getBlockX(), loc.getBlockY(), loc.getBlockZ()).map(Plot::owner)
+                        .orElseGet(()-> chunk.getIsland().map(Island::getCity).map(City::getOwner).orElse(null));
+
+                if(owner == null)
+                    return;
+
+                Entity victim = event.getEntity();
+                BlockPos blockPos = plugin.blockPos(victim.getLocation());
+                FlagHolder holder;
+                if(blockPos.getChunk().equals(chunkPos))
+                    holder = chunk.getFlagHolder(blockPos);
+                else
+                    holder = plugin.mineCity.provideChunk(blockPos.getChunk()).getFlagHolder(blockPos);
+
+                Optional<Message> denial = holder.can(owner,
+                        victim instanceof Player? PVP : victim instanceof Monster? PVM : PVC
+                );
+                if(denial.isPresent())
+                {
+                    event.setCancelled(true);
+                    return;
+                }
+            }
+        }
+
+        if(attacker instanceof Tameable)
+        {
+            AnimalTamer tamer = ((Tameable) attacker).getOwner();
+            if(tamer instanceof Entity)
+                attacker = (Entity) tamer;
+            else if(tamer instanceof OfflinePlayer)
+            {
+                OfflinePlayer offlinePlayer = (OfflinePlayer) tamer;
+                PlayerID owner = new PlayerID(offlinePlayer.getUniqueId(), offlinePlayer.getName());
+
+                Entity victim = event.getEntity();
+                BlockPos blockPos = plugin.blockPos(victim.getLocation());
+                FlagHolder holder = plugin.mineCity.provideChunk(blockPos.getChunk()).getFlagHolder(blockPos);
+
+                Optional<Message> denial = holder.can(owner,
+                        victim instanceof Player? PVP : victim instanceof Monster? PVM : PVC
+                );
+                if(denial.isPresent())
+                {
+                    event.setCancelled(true);
+                    return;
+                }
+            }
+        }
+
+        if(attacker instanceof Player)
+        {
+            Entity victim = event.getEntity();
+            if(check(victim.getLocation(), (Player)attacker, victim instanceof Player? PVP : victim instanceof Monster? PVM : PVC))
+                event.setCancelled(true);
+        }
+    }
+
+    @EventHandler(priority = EventPriority.LOW, ignoreCancelled = true)
+    public void onPlayerInteractAtEntity(PlayerInteractAtEntityEvent event)
+    {
+        Entity entity = event.getRightClicked();
+        Player player = event.getPlayer();
+        if(entity instanceof ArmorStand)
+        {
+            if(check(entity.getLocation(), player, PermissionFlag.OPEN))
+                event.setCancelled(true);
+        }
+    }
+
     @EventHandler(priority = EventPriority.LOW, ignoreCancelled = true)
     public void onPlayerInteractEntity(PlayerInteractEntityEvent event)
     {
@@ -78,8 +336,6 @@ public class VanillaProtections implements Listener
             Villager villager = (Villager) entity;
             if(villager.isAdult() && check(villager.getLocation(), player, PermissionFlag.HARVEST))
                 event.setCancelled(true);
-
-            return;
         }
         else if(entity instanceof Horse)
         {
@@ -123,8 +379,6 @@ public class VanillaProtections implements Listener
                     }
                 }
             }
-
-            return;
         }
         else if(entity instanceof Minecart)
         {
@@ -132,22 +386,16 @@ public class VanillaProtections implements Listener
             {
                 if(check(entity.getLocation(), player, PermissionFlag.ENTER, PermissionFlag.RIDE))
                     event.setCancelled(true);
-
-                return;
             }
             else if(entity instanceof InventoryHolder)
             {
                 if(check(entity.getLocation(), player, PermissionFlag.OPEN))
                     event.setCancelled(true);
-
-                return;
             }
             else if(entity instanceof PoweredMinecart)
             {
                 if(check(entity.getLocation(), player, PermissionFlag.CLICK))
                     event.setCancelled(true);
-
-                return;
             }
             else if(entity instanceof ExplosiveMinecart)
             {
@@ -156,11 +404,7 @@ public class VanillaProtections implements Listener
                     if(check(entity.getLocation(), player, PermissionFlag.MODIFY))
                         event.setCancelled(true);
                 }
-
-                return;
             }
-            else if(entity instanceof SpawnerMinecart)
-                return;
         }
         else if(entity instanceof Cow)
         {
@@ -174,8 +418,6 @@ public class VanillaProtections implements Listener
                         event.setCancelled(true);
                 }
             }
-
-            return;
         }
         else if(entity instanceof Pig)
         {
@@ -196,15 +438,52 @@ public class VanillaProtections implements Listener
                         event.setCancelled(true);
                 }
             }
-
-            return;
         }
         else if(entity instanceof Vehicle)
         {
             if(check(entity.getLocation(), player, PermissionFlag.ENTER, PermissionFlag.RIDE))
                 event.setCancelled(true);
-
-            return;
+        }
+        else if(entity instanceof ItemFrame)
+        {
+            if(check(entity.getLocation(), player, PermissionFlag.OPEN))
+                event.setCancelled(true);
+        }
+        else if(entity instanceof LeashHitch)
+        {
+            if(check(entity.getLocation(), player, PermissionFlag.MODIFY))
+                event.setCancelled(true);
+        }
+        else if(entity.getType() == EntityType.ZOMBIE)
+        {
+            Zombie zombie = (Zombie) entity;
+            if(hand.map(ItemStack::getType).filter(Material.GOLDEN_APPLE::equals).isPresent() && zombie.hasPotionEffect(PotionEffectType.WEAKNESS))
+            {
+                if(check(zombie.getLocation(), player, PermissionFlag.MODIFY))
+                    event.setCancelled(true);
+            }
+        }
+        else if(entity instanceof Tameable)
+        {
+            Tameable tameable = (Tameable) entity;
+            if(!tameable.isTamed())
+            {
+                if(check(entity.getLocation(), player, PermissionFlag.MODIFY))
+                    event.setCancelled(true);
+            }
+        }
+        else if(entity instanceof EnderCrystal)
+        {
+            if(check(entity.getLocation(), player, PermissionFlag.MODIFY))
+                event.setCancelled(true);
+        }
+        else if(entity instanceof LingeringPotion)
+        {
+            if(hand.map(ItemStack::getType).filter(Material.GLASS_BOTTLE::equals).isPresent())
+            {
+                if(check(entity.getLocation(), player, PermissionFlag.CLICK))
+                    event.setCancelled(true);
+            }
         }
     }
 }
