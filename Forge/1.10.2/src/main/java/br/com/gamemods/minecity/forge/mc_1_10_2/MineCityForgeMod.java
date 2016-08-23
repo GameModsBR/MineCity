47c033ebf128c6a8375de4159d82488f67ec25ec
joserobjr <jose.rob.jr@gmail.com>
2016-08-23 20:31:31 -0300

    Forge: Added 1.10.2 and Base modules

---
+++ Forge/1.10.2/src/main/java/br/com/gamemods/minecity/forge/mc_1_10_2/MineCityForgeMod.java
@@ -0,0 +1,8 @@
+package br.com.gamemods.minecity.forge.mc_1_10_2;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = "minecity", name = "MineCity", version = "1.0-SNAPSHOT", acceptableRemoteVersions = "*")
+public class MineCityForgeMod
+{
+}
