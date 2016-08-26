d8b7495c7a7e5a26c8bb7d410d33e21a5caaf618
joserobjr <jose.rob.jr@gmail.com>
2016-08-26 07:15:28 -0300

    Forge: Implements tall grass protection

--- Forge/1.10.2/src/main/java/br/com/gamemods/minecity/forge/mc_1_10_2/accessors/FrostItemDye.java
+++ Forge/1.10.2/src/main/java/br/com/gamemods/minecity/forge/mc_1_10_2/accessors/FrostItemDye.java
@@ -12,6 +12,7 @@ import br.com.gamemods.minecity.forge.mc_1_10_2.core.transformer.forge.FrostItem
 import net.minecraft.block.BlockCrops;
 import net.minecraft.block.BlockOldLog;
 import net.minecraft.block.BlockPlanks;
+import net.minecraft.block.BlockTallGrass;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.EnumDyeColor;
@@ -23,12 +24,16 @@ public interface FrostItemDye extends IItemDye
     default Reaction react(IEntityPlayerMP player, IItemStack stack, boolean offHand, IState state, BlockPos pos,
                            Direction face)
     {
+        assert pos.world.instance != null;
         EnumDyeColor color = EnumDyeColor.byDyeDamage(stack.getMeta());
         if(color == EnumDyeColor.WHITE)
         {
             IBlock block = state.getIBlock();
             if(block instanceof BlockCrops)
                 return new SingleBlockReaction(pos, PermissionFlag.HARVEST);
+            else if(block instanceof BlockTallGrass &&
+                    !(((IWorldServer)pos.world.instance).getIBlock(pos.x, pos.y-1, pos.z) instanceof BlockTallGrass))
+                return new SingleBlockReaction(pos.add(Direction.UP), PermissionFlag.MODIFY);
             else
                 return NoReaction.INSTANCE;
         }
--- Forge/1.7.10/src/main/java/br/com/gamemods/minecity/forge/mc_1_7_10/accessors/SevenItemDye.java
+++ Forge/1.7.10/src/main/java/br/com/gamemods/minecity/forge/mc_1_7_10/accessors/SevenItemDye.java
@@ -10,6 +10,7 @@ import br.com.gamemods.minecity.forge.base.protection.Reaction;
 import br.com.gamemods.minecity.forge.base.protection.SingleBlockReaction;
 import br.com.gamemods.minecity.forge.mc_1_7_10.core.transformer.forge.SevenItemDyeTransformer;
 import net.minecraft.block.BlockCrops;
+import net.minecraft.block.BlockTallGrass;
 import net.minecraft.init.Blocks;
 
 @Referenced(at = SevenItemDyeTransformer.class)
@@ -18,12 +19,16 @@ public interface SevenItemDye extends IItemDye
     @Override
     default Reaction react(IEntityPlayerMP player, IItemStack stack, boolean offHand, IState state, BlockPos pos, Direction face)
     {
+        assert pos.world.instance != null;
         int meta = stack.getMeta();
         if(meta == 15)
         {
             IBlock block = state.getIBlock();
             if(block instanceof BlockCrops)
                 return new SingleBlockReaction(pos, PermissionFlag.HARVEST);
+            else if(block instanceof BlockTallGrass &&
+                    !(((IWorldServer)pos.world.instance).getIBlock(pos.x, pos.y-1, pos.z) instanceof BlockTallGrass))
+                return new SingleBlockReaction(pos.add(Direction.UP), PermissionFlag.MODIFY);
             else
                 return NoReaction.INSTANCE;
         }
