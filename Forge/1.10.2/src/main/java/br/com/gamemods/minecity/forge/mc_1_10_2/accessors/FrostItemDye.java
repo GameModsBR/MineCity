98a06c2f4b1dd393c2612684bfd426887263db11
joserobjr <jose.rob.jr@gmail.com>
2016-08-26 07:05:28 -0300

    Forge: Allows to use bone meal on crops under harvest permission

--- Forge/1.10.2/src/main/java/br/com/gamemods/minecity/forge/mc_1_10_2/accessors/FrostItemDye.java
+++ Forge/1.10.2/src/main/java/br/com/gamemods/minecity/forge/mc_1_10_2/accessors/FrostItemDye.java
@@ -4,14 +4,12 @@ import br.com.gamemods.minecity.api.permission.PermissionFlag;
 import br.com.gamemods.minecity.api.world.BlockPos;
 import br.com.gamemods.minecity.api.world.Direction;
 import br.com.gamemods.minecity.forge.base.Referenced;
-import br.com.gamemods.minecity.forge.base.accessors.IEntityPlayerMP;
-import br.com.gamemods.minecity.forge.base.accessors.IItemDye;
-import br.com.gamemods.minecity.forge.base.accessors.IItemStack;
-import br.com.gamemods.minecity.forge.base.accessors.IState;
+import br.com.gamemods.minecity.forge.base.accessors.*;
 import br.com.gamemods.minecity.forge.base.protection.NoReaction;
 import br.com.gamemods.minecity.forge.base.protection.Reaction;
 import br.com.gamemods.minecity.forge.base.protection.SingleBlockReaction;
 import br.com.gamemods.minecity.forge.mc_1_10_2.core.transformer.forge.FrostItemDyeTransformer;
+import net.minecraft.block.BlockCrops;
 import net.minecraft.block.BlockOldLog;
 import net.minecraft.block.BlockPlanks;
 import net.minecraft.block.state.IBlockState;
@@ -25,7 +23,17 @@ public interface FrostItemDye extends IItemDye
     default Reaction react(IEntityPlayerMP player, IItemStack stack, boolean offHand, IState state, BlockPos pos,
                            Direction face)
     {
-        if(state.getIBlock() != Blocks.LOG || EnumDyeColor.byDyeDamage(stack.getMeta()) != EnumDyeColor.BROWN)
+        EnumDyeColor color = EnumDyeColor.byDyeDamage(stack.getMeta());
+        if(color == EnumDyeColor.WHITE)
+        {
+            IBlock block = state.getIBlock();
+            if(block instanceof BlockCrops)
+                return new SingleBlockReaction(pos, PermissionFlag.HARVEST);
+            else
+                return NoReaction.INSTANCE;
+        }
+
+        if(color != EnumDyeColor.BROWN || state.getIBlock() != Blocks.LOG)
             return NoReaction.INSTANCE;
 
         if(((IBlockState) state).getValue(BlockOldLog.VARIANT) != BlockPlanks.EnumType.JUNGLE)
--- Forge/1.7.10/src/main/java/br/com/gamemods/minecity/forge/mc_1_7_10/accessors/SevenItemDye.java
+++ Forge/1.7.10/src/main/java/br/com/gamemods/minecity/forge/mc_1_7_10/accessors/SevenItemDye.java
@@ -4,14 +4,12 @@ import br.com.gamemods.minecity.api.permission.PermissionFlag;
 import br.com.gamemods.minecity.api.world.BlockPos;
 import br.com.gamemods.minecity.api.world.Direction;
 import br.com.gamemods.minecity.forge.base.Referenced;
-import br.com.gamemods.minecity.forge.base.accessors.IEntityPlayerMP;
-import br.com.gamemods.minecity.forge.base.accessors.IItemDye;
-import br.com.gamemods.minecity.forge.base.accessors.IItemStack;
-import br.com.gamemods.minecity.forge.base.accessors.IState;
+import br.com.gamemods.minecity.forge.base.accessors.*;
 import br.com.gamemods.minecity.forge.base.protection.NoReaction;
 import br.com.gamemods.minecity.forge.base.protection.Reaction;
 import br.com.gamemods.minecity.forge.base.protection.SingleBlockReaction;
 import br.com.gamemods.minecity.forge.mc_1_7_10.core.transformer.forge.SevenItemDyeTransformer;
+import net.minecraft.block.BlockCrops;
 import net.minecraft.init.Blocks;
 
 @Referenced(at = SevenItemDyeTransformer.class)
@@ -20,7 +18,17 @@ public interface SevenItemDye extends IItemDye
     @Override
     default Reaction react(IEntityPlayerMP player, IItemStack stack, boolean offHand, IState state, BlockPos pos, Direction face)
     {
-        if(stack.getMeta() != 3 || state.getIBlock() != Blocks.log)
+        int meta = stack.getMeta();
+        if(meta == 15)
+        {
+            IBlock block = state.getIBlock();
+            if(block instanceof BlockCrops)
+                return new SingleBlockReaction(pos, PermissionFlag.HARVEST);
+            else
+                return NoReaction.INSTANCE;
+        }
+
+        if(meta != 3 || state.getIBlock() != Blocks.log)
             return NoReaction.INSTANCE;
 
         if((state.getStateId() & 3) != 3)
