f4b3e52811294daae355dd99ddef9cad67fc0f4f
joserobjr <jose.rob.jr@gmail.com>
2016-08-30 11:27:43 -0300

    Forge: Fix build issue

--- Forge/1.7.10/src/main/java/br/com/gamemods/minecity/forge/mc_1_7_10/protection/MineCitySevenHooks.java
+++ Forge/1.7.10/src/main/java/br/com/gamemods/minecity/forge/mc_1_7_10/protection/MineCitySevenHooks.java
@@ -24,9 +24,9 @@ public class MineCitySevenHooks
     {
         Event event;
         if(source instanceof Entity)
-            event = new EntityIgniteEntityEvent(entity, (Entity) source, source, sourceClass, method, desc);
+            event = new EntityIgniteEntityEvent(entity, (Entity) source, fireTicks, source, sourceClass, method, desc);
         else
-            event = new EntityIgniteEvent(entity, source, sourceClass, method, desc);
+            event = new EntityIgniteEvent(entity, fireTicks, source, sourceClass, method, desc);
 
         if(!MinecraftForge.EVENT_BUS.post(event))
             entity.setFire(fireTicks);
