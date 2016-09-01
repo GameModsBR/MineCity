e1b4a1ef53276d9aa7dc8c938040b7ef4f0aae59
joserobjr <jose.rob.jr@gmail.com>
2016-09-01 10:12:27 -0300

    Forge: Protects dragon eggs

---
+++ Forge/Base/src/main/java/br/com/gamemods/minecity/forge/base/accessors/block/IBlockDragonEgg.java
@@ -0,0 +1,29 @@
+package br.com.gamemods.minecity.forge.base.accessors.block;
+
+import br.com.gamemods.minecity.api.permission.PermissionFlag;
+import br.com.gamemods.minecity.api.world.BlockPos;
+import br.com.gamemods.minecity.api.world.Direction;
+import br.com.gamemods.minecity.forge.base.Referenced;
+import br.com.gamemods.minecity.forge.base.accessors.entity.base.IEntityPlayerMP;
+import br.com.gamemods.minecity.forge.base.accessors.item.IItemStack;
+import br.com.gamemods.minecity.forge.base.core.transformer.forge.ForgeInterfaceTransformer;
+import br.com.gamemods.minecity.forge.base.protection.reaction.Reaction;
+import br.com.gamemods.minecity.forge.base.protection.reaction.SingleBlockReaction;
+import net.minecraft.block.BlockDragonEgg;
+
+@Referenced(at = ForgeInterfaceTransformer.class)
+public interface IBlockDragonEgg extends IBlock
+{
+    @Override
+    default BlockDragonEgg getForgeBlock()
+    {
+        return (BlockDragonEgg) this;
+    }
+
+    @Override
+    default Reaction reactRightClick(BlockPos pos, IState state, IEntityPlayerMP player, IItemStack stack,
+                                     boolean offHand, Direction face)
+    {
+        return new SingleBlockReaction(pos, PermissionFlag.MODIFY);
+    }
+}
--- Forge/Base/src/main/java/br/com/gamemods/minecity/forge/base/core/transformer/forge/ForgeInterfaceTransformer.java
+++ Forge/Base/src/main/java/br/com/gamemods/minecity/forge/base/core/transformer/forge/ForgeInterfaceTransformer.java
@@ -11,6 +11,9 @@ public class ForgeInterfaceTransformer extends InsertInterfaceTransformer
     {
         Map<String, String> r = new HashMap<>();
 
+        r.put("net.minecraft.block.BlockDragonEgg",
+                "br.com.gamemods.minecity.forge.base.accessors.block.IBlockDragonEgg");
+
         r.put("net.minecraft.entity.passive.EntityOcelot",
                 "br.com.gamemods.minecity.forge.base.accessors.entity.passive.IEntityOcelot");
 
