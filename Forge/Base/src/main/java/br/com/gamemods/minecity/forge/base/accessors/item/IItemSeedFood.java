fd3eb6a8d93959c36cd5db6827a54a70a5567202
joserobjr <jose.rob.jr@gmail.com>
2016-09-14 21:49:39 -0300

    Forge: Consider PamSeedFood as SeedFood #41

--- Forge/Base/src/main/java/br/com/gamemods/minecity/forge/base/accessors/item/IItemSeedFood.java
+++ Forge/Base/src/main/java/br/com/gamemods/minecity/forge/base/accessors/item/IItemSeedFood.java
@@ -7,10 +7,12 @@ import br.com.gamemods.minecity.forge.base.accessors.block.IState;
 import br.com.gamemods.minecity.forge.base.accessors.entity.base.IEntityPlayerMP;
 import br.com.gamemods.minecity.forge.base.core.Referenced;
 import br.com.gamemods.minecity.forge.base.core.transformer.forge.ForgeInterfaceTransformer;
+import br.com.gamemods.minecity.forge.base.core.transformer.mod.ModInterfacesTransformer;
 import br.com.gamemods.minecity.forge.base.protection.reaction.Reaction;
 import br.com.gamemods.minecity.forge.base.protection.reaction.SingleBlockReaction;
 
 @Referenced(at = ForgeInterfaceTransformer.class)
+@Referenced(at = ModInterfacesTransformer.class)
 public interface IItemSeedFood extends IItem
 {
     @Override
--- Forge/Base/src/main/java/br/com/gamemods/minecity/forge/base/core/transformer/mod/ModInterfacesTransformer.java
+++ Forge/Base/src/main/java/br/com/gamemods/minecity/forge/base/core/transformer/mod/ModInterfacesTransformer.java
@@ -15,6 +15,9 @@ public class ModInterfacesTransformer extends InsertInterfaceTransformer
     {
         Map<String, String> r = new HashMap<>();
 
+        r.put("com.pam.harvestcraft.ItemPamSeedFood",
+                "br.com.gamemods.minecity.forge.base.accessors.item.IItemSeedFood");
+
         r.put("com.pam.harvestcraft.BlockPamFruit",
                 "br.com.gamemods.minecity.forge.base.protection.pamharvestcraft.IBlockFruit");
 
