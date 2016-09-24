651aae72b9b88148b0cb5bdd99af8f704b28e385
joserobjr <jose.rob.jr@gmail.com>
2016-09-24 20:41:09 -0300

    Forge: Kill hell bats when they attempt to attack without permission

--- Forge/Base/src/main/java/br/com/gamemods/minecity/forge/base/protection/thaumcraft/IEntityFireBat.java
+++ Forge/Base/src/main/java/br/com/gamemods/minecity/forge/base/protection/thaumcraft/IEntityFireBat.java
@@ -1,17 +1,23 @@
 package br.com.gamemods.minecity.forge.base.protection.thaumcraft;
 
+import br.com.gamemods.minecity.api.command.Message;
+import br.com.gamemods.minecity.api.permission.Permissible;
 import br.com.gamemods.minecity.api.permission.PermissionFlag;
 import br.com.gamemods.minecity.api.world.BlockPos;
+import br.com.gamemods.minecity.forge.base.MineCityForge;
 import br.com.gamemods.minecity.forge.base.accessors.entity.base.IEntity;
 import br.com.gamemods.minecity.forge.base.accessors.entity.base.IEntityLivingBase;
 import br.com.gamemods.minecity.forge.base.accessors.entity.mob.IEntityMob;
+import br.com.gamemods.minecity.forge.base.accessors.item.IItemStack;
 import br.com.gamemods.minecity.forge.base.core.Referenced;
 import br.com.gamemods.minecity.forge.base.core.transformer.mod.ModInterfacesTransformer;
 import net.minecraft.entity.Entity;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.DamageSource;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.List;
 import java.util.UUID;
 
 @Referenced(at = ModInterfacesTransformer.class)
@@ -66,5 +72,14 @@ public interface IEntityFireBat extends IEntityMob
     default Type getType()
     {
         return !getIsSummoned()? Type.MONSTER : Type.PROJECTILE;
+    }
+
+    @Override
+    default void afterPlayerAttack(MineCityForge mod, Permissible player, IItemStack stack, IEntity entity,
+                                   DamageSource source, float amount, List<Permissible> attackers,
+                                   Message message)
+    {
+        if(attackers.contains(this) && getIsSummoned())
+            setDead();
     }
 }
