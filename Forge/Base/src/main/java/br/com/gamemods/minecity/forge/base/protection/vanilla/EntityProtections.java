11aa5395f722beff143e5215e698c5f012abc721
joserobjr <jose.rob.jr@gmail.com>
2016-09-26 02:20:53 -0300

    Forge: Cancel the ignition event if a player was not found

--- Forge/Base/src/main/java/br/com/gamemods/minecity/forge/base/protection/vanilla/EntityProtections.java
+++ Forge/Base/src/main/java/br/com/gamemods/minecity/forge/base/protection/vanilla/EntityProtections.java
@@ -826,7 +826,7 @@ public class EntityProtections extends ForgeProtections
                     return reaction.can(mod.mineCity, player).isPresent();
                 }
 
-                return false;
+                return true;
             }
 
             default:
