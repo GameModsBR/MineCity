8e08b0313920d16ec251baa909a57aad8978e2ea
joserobjr <jose.rob.jr@gmail.com>
2017-01-02 09:56:54 -0300

    Sponge: Fix a Mixin exception

--- Platform/Sponge/src/main/java/br/com/gamemods/minecity/sponge/core/mixin/MixinEntityLivingBase.java
+++ Platform/Sponge/src/main/java/br/com/gamemods/minecity/sponge/core/mixin/MixinEntityLivingBase.java
@@ -4,26 +4,19 @@ import br.com.gamemods.minecity.sponge.core.mixed.MixedEntityLivingBase;
 import net.minecraft.entity.EntityLivingBase;
 import org.spongepowered.api.item.inventory.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(EntityLivingBase.class)
 public abstract class MixinEntityLivingBase implements MixedEntityLivingBase
 {
-    @Shadow
-    public abstract boolean isElytraFlying();
-
-    @Shadow
-    public abstract net.minecraft.item.ItemStack getActiveItemStack();
-
     @Override
     public boolean isFlyingOnElytra()
     {
-        return isElytraFlying();
+        return ((EntityLivingBase) (Object) this).isElytraFlying();
     }
 
     @Override
     public ItemStack getActiveStack()
     {
-        return (ItemStack) (Object) getActiveItemStack();
+        return (ItemStack) (Object) ((EntityLivingBase) (Object) this).getActiveItemStack();
     }
 }
