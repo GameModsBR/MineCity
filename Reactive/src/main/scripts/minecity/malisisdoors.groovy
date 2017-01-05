2e33bef1e2c4e27619d5ddcbc9eea2e2681461fa
joserobjr <jose.rob.jr@gmail.com>
2017-01-05 10:23:18 -0300

    Reactive: Handles special Malisis's doors

--- Reactive/src/main/scripts/minecity/malisisdoors.groovy
+++ Reactive/src/main/scripts/minecity/malisisdoors.groovy
@@ -1,2 +1,19 @@
 package minecity
 
+String block(name) {
+    "net.malisis.doors.block.$name"
+}
+
+blockType([
+        block('BigDoor'), 'malisisdoors:carriage_door', 'malisisdoors:medieval_door',
+        block('RustyHatch'), 'malisisdoors:rustyHatch',
+        block('ModelDoor'), 'malisisdoors:hitechdoor'
+]) {
+    setReactive clickableBlock
+}
+
+blockType([
+        block('SaloonDorBlock'), 'malisisdoors:saloon'
+]) {
+    setReactive decorativeBlock
+}
