6bf5ecab778b793045e82f626348f5ba46979d6d
joserobjr <jose.rob.jr@gmail.com>
2017-01-05 15:07:55 -0300

    Reactive: Handles HarvestCraft fruits and special logs

--- Reactive/src/main/scripts/minecity/harvestcraft/PamRightClickHarvesting.groovy
+++ Reactive/src/main/scripts/minecity/harvestcraft/PamRightClickHarvesting.groovy
@@ -4,13 +4,16 @@ import br.com.gamemods.minecity.api.permission.PermissionFlag
 import br.com.gamemods.minecity.reactive.game.block.*
 import br.com.gamemods.minecity.reactive.game.block.data.BlockRole
 import br.com.gamemods.minecity.reactive.reaction.*
+import com.pam.harvestcraft.HarvestCraft
 
-class PamCrop implements ReactiveBlockType {
+class PamRightClickHarvesting implements ReactiveBlockType {
 
     BlockRole blockRole = BlockRole.HARVESTEABLE
 
     @Override
     Reaction reactRightClick(Interaction event) {
-        new SingleBlockReaction(event.block.position, PermissionFlag.HARVEST)
+        if(HarvestCraft.config.enableEasyHarvest)
+            event.reactionBlock.combineAction(new SingleBlockReaction(event.block.position, PermissionFlag.HARVEST))
+        NoReaction.INSTANCE
     }
 }
--- Reactive/src/main/scripts/minecity/harvestcraft/harvestcraft.groovy
+++ Reactive/src/main/scripts/minecity/harvestcraft/harvestcraft.groovy
@@ -1,13 +1,13 @@
 package minecity.harvestcraft
 
 import com.pam.harvestcraft.blocks.blocks.BlockPamCake
-import com.pam.harvestcraft.blocks.growables.BlockPamCrop
+import com.pam.harvestcraft.blocks.growables.*
 import minecity.minecraft.Cake
 
 blockType(BlockPamCake) {
     setReactive new Cake()
 }
 
-blockType(BlockPamCrop) {
-    setReactive new PamCrop()
+blockType([BlockPamCrop, BlockPamFruit, BlockPamFruitLog]) {
+    setReactive new PamRightClickHarvesting()
 }
