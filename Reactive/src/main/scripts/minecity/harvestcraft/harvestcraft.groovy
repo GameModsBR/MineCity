111c3017972999906ddcd46645f768ea8221c2fd
joserobjr <jose.rob.jr@gmail.com>
2017-01-05 22:04:50 -0300

    Fix: Normal crops can be harvested without protection using harvestcraft

--- Reactive/src/main/scripts/minecity/harvestcraft/harvestcraft.groovy
+++ Reactive/src/main/scripts/minecity/harvestcraft/harvestcraft.groovy
@@ -3,11 +3,12 @@ package minecity.harvestcraft
 import com.pam.harvestcraft.blocks.blocks.BlockPamCake
 import com.pam.harvestcraft.blocks.growables.*
 import minecity.minecraft.Cake
+import net.minecraft.block.BlockCrops
 
 blockType(BlockPamCake) {
     setReactive new Cake()
 }
 
-blockType([BlockPamCrop, BlockPamFruit, BlockPamFruitLog]) {
+blockType([BlockCrops, BlockPamFruit, BlockPamFruitLog]) {
     setReactive new PamRightClickHarvesting()
 }
