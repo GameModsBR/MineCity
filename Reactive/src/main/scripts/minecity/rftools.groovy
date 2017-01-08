438547184dbecd04663385de3aef1150ee9e0bfe
joserobjr <jose.rob.jr@gmail.com>
2017-01-07 21:09:41 -0300

    Handles RFTools blocks

--- Reactive/src/main/scripts/minecity/rftools.groovy
+++ Reactive/src/main/scripts/minecity/rftools.groovy
@@ -1,2 +1,21 @@
 package minecity
 
+import mcjty.rftools.blocks.blockprotector.BlockProtectorBlock
+import mcjty.rftools.blocks.elevator.ElevatorBlock
+import mcjty.rftools.blocks.endergen.*
+import mcjty.rftools.blocks.logic.generic.LogicSlabBlock
+import mcjty.rftools.blocks.monitor.*
+import mcjty.rftools.blocks.relay.RelayBlock
+import mcjty.rftools.blocks.screens.ScreenControllerBlock
+import mcjty.rftools.blocks.teleporter.*
+import mcjty.rftoolscontrol.blocks.multitank.MultiTankBlock
+import mcjty.rftoolscontrol.blocks.node.NodeBlock
+
+blockType([
+        MatterTransmitterBlock, MatterReceiverBlock, DialingDeviceBlock, ScreenControllerBlock,
+        LogicSlabBlock, ElevatorBlock, EnderMonitorBlock, NodeBlock, MultiTankBlock,
+        EndergenicBlock, BlockProtectorBlock, LiquidMonitorBlock, RFMonitorBlock,
+        RelayBlock
+]) {
+    setReactive modifiableBlock
+}
