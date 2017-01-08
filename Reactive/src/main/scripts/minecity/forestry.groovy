5163e9ea2da0841f6912ddb66a34c16a594db229
joserobjr <jose.rob.jr@gmail.com>
2017-01-08 17:01:14 -0300

    Fix: Green house door permission

--- Reactive/src/main/scripts/minecity/forestry.groovy
+++ Reactive/src/main/scripts/minecity/forestry.groovy
@@ -2,7 +2,12 @@ package minecity
 
 import forestry.apiculture.blocks.BlockBeeHives
 import minecity.harvestcraft.PamBeeHive
+import minecity.minecraft.Door
 
 blockType(BlockBeeHives) {
     setReactive new PamBeeHive()
+}
+
+blockType('forestry:greenhouse.door') {
+    setReactive new Door()
 }
