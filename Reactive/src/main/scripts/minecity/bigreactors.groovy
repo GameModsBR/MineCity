4bd24c23e9ebf7831e4cbed84a59a7722b0c66aa
joserobjr <jose.rob.jr@gmail.com>
2017-01-18 18:09:26 -0300

    BigReactors: Fix: Reactors being accessible without permission

--- Reactive/src/main/scripts/minecity/bigreactors.groovy
+++ Reactive/src/main/scripts/minecity/bigreactors.groovy
@@ -5,3 +5,9 @@ import minecity.forge.Wrench
 itemType('bigreactors:wrench') {
     setReactive new Wrench()
 }
+
+blockType([
+        'bigreactors:reactorController', 'bigreactors:reactorAccessPort'
+]) {
+    setReactive modifiableBlock
+}
